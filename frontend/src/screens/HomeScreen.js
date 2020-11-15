f5b128724f63a232227d277cf124042bc4a24336
Allah-The-Dev <pandeyneeleshkumar@gmail.com>
2020-11-15 19:15:30 +0530

    code alignment

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,34 +7,34 @@ import MessageBox from '../components/MessageBox';
 export default function HomeScreen() {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  useEffect(() => {
-    const fecthData = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get('/api/products');
-        setLoading(false);
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-    fecthData();
-  }, []);
-    return (
-        <div>
-            {loading ? (
-                <LoadingBox></LoadingBox>
-            ) : error ? (
-                <MessageBox variant="danger">{error}</MessageBox>
-            ) : (
-                <div className="row center">
-                {products.map((product) => (
-                    <Product key={product._id} product={product}></Product>
-                ))}
-                </div>
-            )}
-        </div>
-    )
+    const [error, setError] = useState(false);
+    useEffect(() => {
+        const fecthData = async () => {
+        try {
+            setLoading(true);
+            const { data } = await axios.get('/api/products');
+            setLoading(false);
+            setProducts(data);
+        } catch (err) {
+            setError(err.message);
+            setLoading(false);
+        }
+        };
+        fecthData();
+    }, []);
+        return (
+            <div>
+                {loading ? (
+                    <LoadingBox></LoadingBox>
+                ) : error ? (
+                    <MessageBox variant="danger">{error}</MessageBox>
+                ) : (
+                    <div className="row center">
+                    {products.map((product) => (
+                        <Product key={product._id} product={product}></Product>
+                    ))}
+                    </div>
+                )}
+            </div>
+        )
 }
