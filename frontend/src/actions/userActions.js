2847333f6c866b75013eb782e312695134b41852
Allah-The-Dev <pandeyneeleshkumar@gmail.com>
2020-11-17 17:17:20 +0530

    1. user register api 2. user register screen 3. user register redux

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,10 +1,36 @@
 import Axios from 'axios';
+import { USER_REGISTER_FAIL } from '../constants/userConstants';
+import { USER_REGISTER_SIGNOUT } from '../constants/userConstants';
+import { USER_REGISTER_SUCCESS } from '../constants/userConstants';
+import { USER_REGISTER_REQUEST } from '../constants/userConstants';
 import {
   USER_SIGNIN_FAIL,
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SUCCESS,
   USER_SIGNOUT,
 } from '../constants/userConstants';
+
+export const register = (name, email, password) => async (dispatch) => {
+    dispatch({ type: USER_REGISTER_REQUEST, payload: { email, password } });
+    try {
+      const { data } = await Axios.post('/api/users/register', {
+        name,
+        email,
+        password,
+      });
+      dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+      dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (error) {
+      dispatch({
+        type: USER_REGISTER_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+};
 
 export const signin = (email, password) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
@@ -27,4 +53,5 @@ export const signout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   localStorage.removeItem('cartItems');
   dispatch({ type: USER_SIGNOUT });
+  dispatch({ type: USER_REGISTER_SIGNOUT });
 };
