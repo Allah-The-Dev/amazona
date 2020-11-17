35d50ef65f36c7c7d69203e3fdc9d5dcedc506d1
Allah-The-Dev <pandeyneeleshkumar@gmail.com>
2020-11-17 16:48:13 +0530

    sign in and log out implemented

--- backend/router/user.js
+++ backend/router/user.js
@@ -3,6 +3,7 @@ import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import User from '../models/user.js';
 import bcrypt from 'bcryptjs';
+import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
