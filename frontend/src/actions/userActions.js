b5dcf022bcae49925712ef0d521e4e049e9f5d75
Allah-The-Dev <pandeyneeleshkumar@gmail.com>
2020-11-22 19:04:30 +0530

    paymentmethod screen, payment redux

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -52,6 +52,7 @@ export const signin = (email, password) => async (dispatch) => {
 export const signout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   localStorage.removeItem('cartItems');
+  localStorage.removeItem('shippingAddress');
   dispatch({ type: USER_SIGNOUT });
   dispatch({ type: USER_REGISTER_SIGNOUT });
 };
